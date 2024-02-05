fc4adf9127ea39410a028818d426908c97dfda20
Zegnar0 <zegnarkow@gmail.com>
2024-02-05 16:54:49 +0100

    Card and Basic Fetch Implementation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
+     <div id="app">
+        <Card
+        v-for="(rocket, index) in rockets"
+      :key="index"
+      :imageUrl="rocket.flickr_images[0]"
+      :title="rocket.name"
+      :description="rocket.description"
+    />
+    </div>
   
 </template>
 
 <script>
-export default{
+import Card from './components/Card.vue';
 
-}
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      rockets: [],
+    };
+  },
+  mounted() {
+
+    this.fetchRocketData();
+  },
+  methods: {
+    async fetchRocketData() {
+      try {
+        const response = await fetch('https://api.spacexdata.com/v4/rockets');
+        const data = await response.json();
+        this.rockets = data;
+      } catch (error) {
+        console.error('Error fetching rocket data:', error);
+      }
+    },
+  },
+};
 </script>
+
